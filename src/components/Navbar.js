946ed84f8cf6f9939e068367a98c3e41fc69ad08
Daniel Peters <89535810+drpeters4@users.noreply.github.com>
2022-03-31 12:27:41 -0500

    Commiting page updates

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <div className="Navbar">
+            <nav>
+                <Link to="/">Intro</Link>
+                <Link to="/home">Home</Link>
+                <Link to="/profile">Profile</Link>
+                <Link to="/createSurvey">Create Survey</Link>
+                <Link to="/mySurveys">My Surveys</Link>
+                <Link to="/stats">Stats</Link>
+            </nav>
+        </div>
+    )
+}
