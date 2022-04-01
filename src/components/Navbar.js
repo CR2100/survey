1e9a541d3e3a490bc3c7eaf684a6de0860fff8f7
crander3 <crander3@go.olemiss.edu>
2022-04-01 14:15:17 -0500

    edited intro.js and navbar formats

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const linkStyle = {
+    color: 'orange',
+    padding: 10,
+  };
 export default function Navbar() {
     return (
+        
         <div className="Navbar">
             <nav>
-                <Link to="/">Intro</Link>
-                <Link to="/home">Home</Link>
-                <Link to="/profile">Profile</Link>
-                <Link to="/createSurvey">Create Survey</Link>
-                <Link to="/mySurveys">My Surveys</Link>
-                <Link to="/stats">Stats</Link>
+                <Link to="/" style={linkStyle}>Intro</Link>
+                <Link to="/home" style={linkStyle}>Home</Link>
+                <Link to="/profile" style={linkStyle}>Profile</Link>
+                <Link to="/createSurvey" style={linkStyle}>Create Survey</Link>
+                <Link to="/mySurveys" style={linkStyle}>My Surveys</Link>
+                <Link to="/stats" style={linkStyle}>Stats</Link>
             </nav>
         </div>
     )
