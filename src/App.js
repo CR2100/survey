3bf7ca1d1fcaa3f7d1eeddf7813c96b38ebd2ab6
Daniel R. Peters <drpeters@go.olemiss.edu>
2022-04-05 21:50:10 -0500

    Commiting basic additions to MySurveys.js & App.js. Further additions will need access to mySQL queries.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Intro from './pages/Intro';
 import Home from './pages/Home';
@@ -7,6 +8,10 @@ import CreateSurvey from './pages/CreateSurvey';
 import MySurveys from './pages/MySurveys';
 import Stats from './pages/Stats';
 import Navbar from './components/Navbar';
+
+// Using state to keep track of which user is logged in. Both the currUser variable and setCurrUser function will need to be passed as props 
+  // to the file handles login / logout / register.
+const [currUser, setCurrUser] = useState({});
 
 function App() {
   return (
@@ -18,7 +23,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/createSurvey" element={<CreateSurvey />} />
-          <Route path="/mySurveys" element={<MySurveys />} />
+          <Route path="/mySurveys" element={<MySurveys currUser={currUser} />} />
           <Route path="/stats" element={<Stats />} />
         </Routes>
       </Router>
