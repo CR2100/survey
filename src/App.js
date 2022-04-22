fcd94698504bdbc914f064caba1f085200c56382
crander3 <crander3@go.olemiss.edu>
2022-04-21 22:17:45 -0500

    changed createsurvey page to 2d array, reformatted each file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Intro from './pages/Intro';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import CreateSurvey from './pages/CreateSurvey';
-import MySurveys from './pages/MySurveys';
-import Stats from './pages/Stats';
-import Navbar from './components/Navbar';
-import LoginPage from './pages/LoginPage';
+import Intro from "./pages/Intro";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import CreateSurvey from "./pages/CreateSurvey";
+import MySurveys from "./pages/MySurveys";
+import Stats from "./pages/Stats";
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
 function App() {
   const [currUser, setCurrUser] = useState({});
   return (
@@ -20,7 +20,10 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/createSurvey" element={<CreateSurvey />} />
-          <Route path="/mySurveys" element={<MySurveys currUser={currUser} />} />
+          <Route
+            path="/mySurveys"
+            element={<MySurveys currUser={currUser} />}
+          />
           <Route path="/stats" element={<Stats />} />
           <Route path="/loginPage" element={<LoginPage />} />
         </Routes>
